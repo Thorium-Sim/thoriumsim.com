d1251cbd0771a2e72930ed219b07ea989cfe5e6d
Alex Anderson <hey@ralexanderson.com>
2025-01-07 19:43:22 -0700

    Disable password reset.

--- app/routes/password-reset.tsx
+++ app/routes/password-reset.tsx
@@ -4,17 +4,17 @@ import { db } from "~/helpers/prisma.server";
 import { hash } from "bcryptjs";
 import { emailSender } from "~/helpers/email";
 import {
-  ActionFunction,
-  MetaFunction,
+  type ActionFunction,
+  type MetaFunction,
   redirect,
 } from "@remix-run/server-runtime";
 import { commitSession, getSession } from "~/session.server";
 import { Form, Link, useTransition } from "@remix-run/react";
 
-export let action: ActionFunction = async ({ request }) => {
-  let session = await getSession(request);
+export const action: ActionFunction = async ({ request }) => {
+  const session = await getSession(request);
   try {
-    let bodyParams = new URLSearchParams(await request.text());
+    const bodyParams = new URLSearchParams(await request.text());
     const email = bodyParams.get("email");
     if (!email) throw new Error("Email is required");
     const user = await db.user.findFirst({ where: { email } });
@@ -27,28 +27,28 @@ export let action: ActionFunction = async ({ request }) => {
         passwordResetExpire: new Date(Date.now() + 1000 * 60 * 60),
       },
     });
-    emailSender.sendMail({
-      to: email,
-      from: '"Alex from Thorium" hey@thoriumsim.com',
-      subject: "Reset your password",
-      html: `
-    <p>
-      You have requested to reset your password.
-    </p>
-    <p>
-      <a href="https://thoriumsim.com/new-password?token=${token}">Click here to reset your password</a>
-    </p>
-    <p>
-      If you did not request to reset your password, please ignore this email.
-    </p>
-    <p>
-      Thanks,
-      <br />
-      Alex
-    </p>
-    `,
-    });
-    session.flash("toast", "Check your email inbox for reset instructions.");
+    // emailSender.sendMail({
+    //   to: email,
+    //   from: '"Alex from Thorium" hey@thoriumsim.com',
+    //   subject: "Reset your password",
+    //   html: `
+    // <p>
+    //   You have requested to reset your password.
+    // </p>
+    // <p>
+    //   <a href="https://thoriumsim.com/new-password?token=${token}">Click here to reset your password</a>
+    // </p>
+    // <p>
+    //   If you did not request to reset your password, please ignore this email.
+    // </p>
+    // <p>
+    //   Thanks,
+    //   <br />
+    //   Alex
+    // </p>
+    // `,
+    // });
+    // session.flash("toast", "Check your email inbox for reset instructions.");
   } catch (e: unknown) {
     if (e instanceof Error) {
       session.flash("error", e.message);
@@ -67,18 +67,19 @@ export const meta: MetaFunction = () => ({
 });
 
 export default function PasswordReset() {
-  let pendingForm = useTransition().submission;
+  const pendingForm = useTransition().submission;
 
   return (
     <div className="mt-32 flex min-h-full w-full flex-col items-center justify-center">
       <div className="w-full max-w-xl flex-1">
         <h1 className="text-5xl font-bold">Reset your Password</h1>
+        <p>Password reset is temporarily disabled.</p>
         <div className="h-16"></div>
         <Form
           className="mb-16 flex flex-col rounded bg-tgray-500 bg-opacity-50 px-8 pt-6 pb-8 shadow-md"
           method="post"
         >
-          <Input
+          {/* <Input
             label="Email"
             type="email"
             name="email"
@@ -92,7 +93,7 @@ export default function PasswordReset() {
               disabled={!!pendingForm}
             >
               {pendingForm ? <FaSpinner className="animate-spin" /> : "Sign In"}
-            </button>
+            </button> */}
           </div>
           <hr className="mx-auto my-8 w-1/2" />
           <p className="text-center">
